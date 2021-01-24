de037b662066e3de207713234f585ee011991622
RAMASUBRAMANIYAN G <gitbash.hub@gmail.com>
2021-01-24 20:49:43 +0530

    Final Commit
    Final Commit

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>
+                © 2021 Airbnb, Inc. No rights reserved. This is a demo!
+            </p>
+            <p>
+                Privacy ⋅ Terms ⋅ SiteMap ⋅ Company Details
+            </p>
+        </div>
+    )
+}
+
+export default Footer
